587d2f7762d57deb4b1dcdc9ecb2297c86aaa3b9
happytallon <35655545+happytallon@users.noreply.github.com>
2018-10-01 17:31:41 -0300

    Criação do Projeto
    Criando projeto do teste da Fullbar.

---
+++ Teste_Backend/Teste_Backend/Models/TbCurso.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Teste_Backend.Models
+{
+    public partial class TbCurso
+    {
+        public TbCurso()
+        {
+            TbAluno = new HashSet<TbAluno>();
+        }
+        public int IdCurso { get; set; }
+        public string Curso { get; set; }
+
+        public TbCurso IdCursoNavigation { get; set; }
+        public TbCurso InverseIdCursoNavigation { get; set; }
+        public ICollection<TbAluno> TbAluno { get; set; }
+    }
+}
---
+++ Teste_Backend/Teste_Backend/Models/TbPeriodo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Teste_Backend.Models
+{
+    public partial class TbPeriodo
+    {
+        public TbPeriodo()
+        {
+            TbAluno = new HashSet<TbAluno>();
+        }
+
+        public int IdPeriodo { get; set; }
+        public string Periodo { get; set; }
+
+        public ICollection<TbAluno> TbAluno { get; set; }
+    }
+}
